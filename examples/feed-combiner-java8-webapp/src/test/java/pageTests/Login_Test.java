3d06f8a23e76402a8bc92d1b56c25cf7199d628e
devops81 <devops81@gmail.com>
2017-05-12 14:25:29 +0530

    Update Login_Test.java

--- examples/feed-combiner-java8-webapp/src/test/java/pageTests/Login_Test.java
+++ examples/feed-combiner-java8-webapp/src/test/java/pageTests/Login_Test.java
@@ -48,7 +48,8 @@ public class Login_Test {
 	@Test
 	public void verifySubscriberNewsletter() throws InterruptedException {
 Thread.sleep(30000);
-driver.findElement(By.xpath("//a[@id='newsletter']")).click();
+driver.findElement(By.linkText("Demo-github-Proj")).click();
+//driver.findElement(By.xpath("//a[@id='newsletter']")).click();
 
 driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Pradeep Chand Nailwal");
 
